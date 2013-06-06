83105eb75ada55a28f530edb6474c32246abed5b
Dan Mazur <dan.mazur@gmail.com>
2013-06-06 15:52:52 -0500

    Syntax error fixup

--- manifests/set_root_passwd.pp
+++ manifests/set_root_passwd.pp
@@ -1,11 +1,11 @@
-# Public: Create MySQL Databases
+# Public: Set MySQL Database root user password
 #
 # namevar - The value of the root user's password.
 #
 # Examples
 #
 #   mysql::set_root_passwd { 'd0ntBsn00pinMyPwd': }
-define mysql::set_root_passwd {
+define mysql::set_root_passwd() {
   require mysql
 
   exec { "create mysql db ${name}":

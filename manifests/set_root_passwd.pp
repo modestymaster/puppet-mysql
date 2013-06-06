d1ff6c7a7c13081a08b4afae6028f67116227834
Dan Mazur <dan.mazur@gmail.com>
2013-06-06 16:46:56 -0500

    updated exec output

--- manifests/set_root_passwd.pp
+++ manifests/set_root_passwd.pp
@@ -8,7 +8,7 @@
 define mysql::set_root_passwd() {
   require mysql
 
-  exec { "create mysql db ${name}":
+  exec { "Setting mysql root user's password to: ${name}":
     command => "mysqladmin -u root password ${name}",
     require => Exec['wait-for-mysql']
   }

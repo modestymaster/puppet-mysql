e1695c926ca617be630f2843a95af85b60ecb4fc
Dan Mazur <dan.mazur@gmail.com>
2013-06-11 14:28:02 -0500

    Setting the mysqladmin command with base options conditionally, based on the presence of the root passwd in config

--- manifests/db.pp
+++ manifests/db.pp
@@ -7,16 +7,25 @@
 #   mysql::db { 'foo': }
 define mysql::db($ensure = present) {
   require mysql
+  require mysql::config
+ 
+  $passwd = getvar(mysql::config::rootpasswd)
+
+  if empty($passwd) {
+    $basecmd = "mysqladmin -u root"
+  } else {
+    $basecmd = "mysqladmin -u root -p${passwd}"
+  }
 
   if $ensure == 'present' {
     exec { "create mysql db ${name}":
-      command => "mysqladmin -uroot create ${name}",
+      command => "${basecmd} create ${name}",
       creates => "${mysql::config::datadir}/${name}",
       require => Exec['wait-for-mysql']
     }
   } elsif $ensure == 'absent' {
     exec { "delete mysql db ${name}":
-      command => "mysqladmin -uroot drop ${name}",
+      command => "${basecmd} -f drop ${name}",
       require => Exec['wait-for-mysql']
     }
   }
